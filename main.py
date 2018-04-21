41512940068143d0cce1dc2eb9d2361569d55897
Ilya Chistyakov <ilchistyakov@gmail.com>
2018-04-21 19:36:44 +0700

    some refactoring and add auto join

--- main.py
+++ main.py
@@ -8,123 +8,149 @@ from configparser import ConfigParser
 
 from telethon import TelegramClient, events
 from telethon.tl.functions import channels
+from telethon.tl.functions.messages import ImportChatInviteRequest
 
 logging.basicConfig(format="%(message)s", level=logging.WARNING)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-if len(sys.argv) == 1:
-    config_file = "config.ini"
-elif len(sys.argv) == 2:
-    config_file = sys.argv[1]
-else:
-    sys.exit("Error: command line arguments are not valid.")
 
-config = ConfigParser()
-config.read(config_file)
+class Config:
+    def __init__(self, config_name):
+        config_parser = ConfigParser()
+        config_parser.read(config_name)
 
-api_id = config.getint("telethon", "api_id")
-api_hash = config.get("telethon", "api_hash")
-session_name = config.get("telethon", "session_name", fallback="default")
+        self.api_id = config_parser.getint("telethon", "api_id")
+        self.api_hash = config_parser.get("telethon", "api_hash")
+        self.session_name = config_parser.get("telethon", "session_name",
+                                              fallback="default")
 
-client = TelegramClient(session_name, api_id, api_hash,
-                        update_workers=4, spawn_read_thread=False)
-client.start()
+        self.forwarder_channels = set(
+            c.strip() for c in
+            config_parser.get("forwarder", "channels",
+                              fallback="").split(","))
 
-forwarder_channels = [client.get_input_entity(c.strip())
-                      for c in config.get("forwarder", "channels",
-                                          fallback="").split(",")]
-patterns = [p.strip() for p in config.get("forwarder", "patterns",
-                                          fallback="").split(",")]
-recipient = client.get_input_entity(config.get("forwarder", "recipient",
-                                               fallback="me").strip())
+        self.patterns = [
+            p.strip() for p in config_parser.get("forwarder", "patterns",
+                                                 fallback="").split(",")]
 
-downloader_channels = [client.get_input_entity(c.strip())
-                       for c in config.get("downloader", "channels",
-                                           fallback="").split(",")]
-download_directory = config.get("downloader", "download_directory",
-                                fallback="./")
-if not download_directory.endswith("/"):
-    download_directory += "/"
-os.makedirs(download_directory, exist_ok=True)
-download_audios = config.getboolean("downloader", "download_audios",
-                                    fallback=True)
-download_documents = config.getboolean("downloader", "download_documents",
-                                       fallback=True)
-download_gifs = config.getboolean("downloader", "download_gifs",
-                                  fallback=True)
-download_photos = config.getboolean("downloader", "download_photos",
-                                    fallback=True)
-download_stickers = config.getboolean("downloader", "download_stickers",
-                                      fallback=True)
-download_videos = config.getboolean("downloader", "download_videos",
-                                    fallback=True)
-download_video_notes = config.getboolean("downloader", "download_video_notes",
-                                         fallback=True)
-download_voices = config.getboolean("downloader", "download_voices",
-                                    fallback=True)
+        self.recipient = config_parser.get("forwarder", "recipient",
+                                           fallback="me").strip()
 
-if not forwarder_channels and downloader_channels:
-    sys.exit("sys.exiting, there is no channel to watch.")
+        self.downloader_channels = set(
+            c.strip() for c in
+            config_parser.get("downloader", "channels",
+                              fallback="").split(","))
 
-if forwarder_channels:
-    @client.on(events.NewMessage(incoming=True, chats=forwarder_channels))
-    @client.on(events.MessageEdited(incoming=True, chats=forwarder_channels))
-    def forwarder(event):
-        logger.debug("Received a message from a channel in forwarder list")
-        client(channels.ReadHistoryRequest(event.input_chat, event.message.id))
-        logger.debug("Marked the message as read")
-        for pattern in patterns:
-            if re.search(pattern, event.raw_text, re.I) is not None:
-                logger.info("The message matches a pattern, forwarding it")
-                return event.forward_to(recipient)
+        self.download_directory = config_parser.get("downloader",
+                                                    "download_directory",
+                                                    fallback="./")
+        if not self.download_directory.endswith("/"):
+            self.download_directory += "/"
 
-if downloader_channels:
-    @client.on(events.NewMessage(incoming=True, chats=downloader_channels))
-    def downloader(event):
-        logger.debug("Received a message from a channel in downloader list")
-        client(channels.ReadHistoryRequest(event.input_chat, event.message.id))
-        logger.debug("Marked the message as read")
-        if event.audio:
-            if download_audios:
-                location = client.download_media(event.message,
-                                                 download_directory)
-                logger.info("Audio downloaded to " + location)
-        elif event.gif:
-            if download_gifs:
-                location = client.download_media(event.message,
-                                                 download_directory)
-                logger.info("GIF downloaded to " + location)
-        elif event.photo:
-            if download_photos:
-                location = client.download_media(event.message,
-                                                 download_directory)
-                logger.info("Photo downloaded to " + location)
-        elif event.sticker:
-            if download_stickers:
-                location = client.download_media(event.message,
-                                                 download_directory)
-                logger.info("Sticker downloaded to " + location)
-        elif event.video:
-            if download_videos:
-                location = client.download_media(event.message,
-                                                 download_directory)
-                logger.info("Video downloaded to " + location)
-        elif event.video_note:
-            if download_video_notes:
-                location = client.download_media(event.message,
-                                                 download_directory)
-                logger.info("Video note downloaded to " + location)
-        elif event.voice:
-            if download_voices:
-                location = client.download_media(event.message,
-                                                 download_directory)
-                logger.info("Voice downloaded to " + location)
-        elif event.document:
-            if download_documents:
-                location = client.download_media(event.message,
-                                                 download_directory)
-                logger.info("Document downloaded to " + location)
+        self.download_audios = config_parser.getboolean(
+            "downloader", "download_audios", fallback=True)
+        self.download_documents = config_parser.getboolean(
+            "downloader", "download_documents", fallback=True)
+        self.download_gifs = config_parser.getboolean(
+            "downloader", "download_gifs", fallback=True)
+        self.download_photos = config_parser.getboolean(
+            "downloader", "download_photos", fallback=True)
+        self.download_stickers = config_parser.getboolean(
+            "downloader", "download_stickers", fallback=True)
+        self.download_videos = config_parser.getboolean(
+            "downloader", "download_videos", fallback=True)
+        self.download_video_notes = config_parser.getboolean(
+            "downloader", "download_video_notes", fallback=True)
+        self.download_voices = config_parser.getboolean(
+            "downloader", "download_voices", fallback=True)
 
-print("Listening for new messages...")
-client.idle()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        config_file = "config.ini"
+    elif len(sys.argv) == 2:
+        config_file = sys.argv[1]
+    else:
+        sys.exit("Error: command line arguments are not valid.")
+
+    config = Config(config_file)
+
+    os.makedirs(config.download_directory, exist_ok=True)
+
+    client = TelegramClient(config.session_name,
+                            config.api_id, config.api_hash,
+                            update_workers=4, spawn_read_thread=False)
+    client.start()
+
+    forwarder_channels = []
+
+    for c_name in config.forwarder_channels:
+        try:
+            channel = client.get_input_entity(c_name)
+        except ValueError as e:
+            if 'Join the group' in str(e):
+                invite = c_name.rsplit('/', 1)[-1]
+                client(ImportChatInviteRequest(invite))
+                channel = client.get_input_entity(c_name)
+            else:
+                raise
+
+        forwarder_channels.append(channel)
+
+    recipient = client.get_input_entity(config.recipient)
+
+    downloader_channels = [client.get_input_entity(c)
+                           for c in config.downloader_channels]
+
+    if not forwarder_channels and downloader_channels:
+        sys.exit("sys.exiting, there is no channel to watch.")
+
+    if forwarder_channels:
+        @client.on(events.NewMessage(incoming=True, chats=forwarder_channels))
+        @client.on(events.MessageEdited(incoming=True,
+                                        chats=forwarder_channels))
+        def forwarder(event):
+            logger.debug("Received a message from a channel in forwarder list")
+            client(channels.ReadHistoryRequest(event.input_chat,
+                                               event.message.id))
+            logger.debug("Marked the message as read")
+            for pattern in config.patterns:
+                if re.search(pattern, event.raw_text, re.I) is not None:
+                    logger.info("The message matches a pattern, forwarding it")
+                    return event.forward_to(recipient)
+
+    if downloader_channels:
+        event_mapper = []
+        if config.download_audios:
+            event_mapper.append(('audio', 'Audio'))
+        if config.download_gifs:
+            event_mapper.append(('gif', 'GIF'))
+        if config.download_photos:
+            event_mapper.append(('photo', 'Photo'))
+        if config.download_stickers:
+            event_mapper.append(('sticker', 'Sticker'))
+        if config.download_videos:
+            event_mapper.append(('video', 'Video'))
+        if config.download_video_notes:
+            event_mapper.append(('video_note', 'Video note'))
+        if config.download_voices:
+            event_mapper.append(('voice', 'Voice'))
+        if config.download_documents:
+            event_mapper.append(('document', 'Document'))
+
+        @client.on(events.NewMessage(incoming=True, chats=downloader_channels))
+        def downloader(event):
+            logger.debug("Received a message "
+                         "from a channel in downloader list")
+            client(channels.ReadHistoryRequest(event.input_chat,
+                                               event.message.id))
+            logger.debug("Marked the message as read")
+            for key, name in event_mapper:
+                if getattr(event, key):
+                    location = client.download_media(event.message,
+                                                     config.download_directory)
+                    logger.info("{} downloaded to {}".format(name, location))
+
+    print("Listening for new messages...")
+    client.idle()
